964ec7f1b923c2c9396c9b0a265f994bb829b98d
Francine Dash <fdash@iu.edu>
2019-08-26 16:00:26 -0400

    projects
    presentation projects

---
+++ precache-manifest.36c7732f04ba006ea375bb38a4c78a99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a690e6d8c936b99fa604860e919b043",
+    "url": "/fdash/presentation/index.html"
+  },
+  {
+    "revision": "540d12ba36c85a1f9090",
+    "url": "/fdash/presentation/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "3de19ded15772457434d",
+    "url": "/fdash/presentation/static/js/2.3cb6ca82.chunk.js"
+  },
+  {
+    "revision": "540d12ba36c85a1f9090",
+    "url": "/fdash/presentation/static/js/main.d9bda743.chunk.js"
+  },
+  {
+    "revision": "0086180faacca36ca42b",
+    "url": "/fdash/presentation/static/js/runtime~main.9062682b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fdash/presentation/static/media/logo.5d5d9eef.svg"
+  }
+]);
